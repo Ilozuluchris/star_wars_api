6b6662dfdfcdc4dc7aa8bb155cecafa4b2f3df6e
ilozuluchris <chidiuso@grit.systems>
2019-10-22 22:58:28 +0100

    Single film resource working: can serialize data for a single firm

--- app/Http/Controllers/FilmsController.php
+++ app/Http/Controllers/FilmsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\SwApiException;
+use App\Http\Resources\Film;
+use App\Http\Resources\FilmCollection;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
@@ -25,8 +27,14 @@ class FilmsController extends Controller
             throw  new SwApiException('Error getting list of films from swapi.com');
         }
         $contents = $res->getBody()->getContents();
-        $json_content = json_decode($contents);
-        return response()->json($json_content, 200);
+        $json_content =  json_decode($contents, true);
+        $results = $json_content['results'][0];
+//        echo gettype($results);
+//        dd($results);
+//        return new Film($results);
+//        dd($json_content);
+//        return response()->json($json_content, 200);
+        return new Film($results);
     }
 
 }
