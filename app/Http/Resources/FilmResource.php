a8d1d54b6d4d7826d6f3b4aea59ee3645ab1fdba
ilozuluchris <chidiuso@grit.systems>
2019-10-23 01:30:28 +0100

    Stub method for getting count of comments

--- app/Http/Resources/FilmResource.php
+++ app/Http/Resources/FilmResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use phpDocumentor\Reflection\Types\Integer;
 
 class FilmResource extends JsonResource
 {
@@ -15,11 +16,17 @@ class FilmResource extends JsonResource
     public function toArray($request)
     {
 
-       return [
+        $episode_id = $this->resource['episode_id'];
+        return [
            'title'=>$this->resource['title'],
-           'episode_id'=>$this->resource['episode_id'],
+           'episode_id'=> $episode_id,
            'release_date'=>$this->resource['release_date'],
-           'comment_count'=>0
+           'comment_count'=>$this->getCommentCount($episode_id)
        ];
+    }
+
+    private function getCommentCount($episode_id){
+        #todo
+        return 0;
     }
 }
