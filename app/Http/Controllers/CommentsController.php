d567c9a25a23a418442dbeef623c582331fb4156
ilozuluchris <chidiuso@grit.systems>
2019-10-25 17:10:36 +0100

    Returning json after saved comment

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -13,82 +13,20 @@ class CommentsController extends Controller
     {
         $this->repository = $commentRepository;
     }
-//    /**
-//     * Display a listing of the resource.
-//     *
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function index()
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Show the form for creating a new resource.
-//     *
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function create()
-//    {
-//        //
-//    }
+
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created comment in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
-
-        $this->repository->save($request->all());
+        $saved_comment = $this->repository->save($request->all());
+        return response()->json(
+            ['data'=>$saved_comment], 201
+        );
     }
-//
-//    /**
-//     * Display the specified resource.
-//     *
-//     * @param  \App\Comment  $comment
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function show(CommentRepositoryInterface $comment)
-//    {
-//        //
-//        $comment->
-//    }
 
-//    /**
-//     * Show the form for editing the specified resource.
-//     *
-//     * @param  \App\Comment  $comment
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function edit(Comment $comment)
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Update the specified resource in storage.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @param  \App\Comment  $comment
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function update(Request $request, Comment $comment)
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Remove the specified resource from storage.
-//     *
-//     * @param  \App\Comment  $comment
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function destroy(Comment $comment)
-//    {
-//        //
-//    }
 }
