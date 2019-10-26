bec83e1694df22049d238c348a7c6324d97d0270
ilozuluchris <chidiuso@grit.systems>
2019-10-26 11:26:29 +0100

    adding total height and  total count to list of characters

--- app/Http/Resources/CharacterResourceCollection.php
+++ app/Http/Resources/CharacterResourceCollection.php
@@ -15,7 +15,29 @@ class CharacterResourceCollection extends ResourceCollection
     public function toArray($request)
     {
         return [
-            'data'=>$this->collection
+            'data'=>$this->collection,
+            'meta'=>[
+                'total_count'=>$this->collection->count(),
+                'total_height'=>$this->totalHeight($this->collection)
+            ]
         ];
+    }
+
+    private function totalHeight($collection)
+    {
+        $total_height_cm = $collection->sum('height');
+        $feet_inches = $this->cm2feet($total_height_cm);
+        return ['cm'=>$total_height_cm,'feet'=>$feet_inches];
+
+
+    }
+
+    private function cm2feet(int $cm)
+    {
+        $inches = $cm/2.54;
+        $feet = intdiv($inches, 12);
+        $inches = $inches%12;
+        return ['feet'=>$feet,'inches'=>$inches];
+
     }
 }
