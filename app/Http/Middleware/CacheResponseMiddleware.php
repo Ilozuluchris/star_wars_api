ddb859015c7b78e05f8cfab3b0fde73b2790b479
ilozuluchris <chidiuso@grit.systems>
2019-10-27 18:03:07 +0100

    Adding status code returns nothing

--- app/Http/Middleware/CacheResponseMiddleware.php
+++ app/Http/Middleware/CacheResponseMiddleware.php
@@ -14,17 +14,30 @@ class CacheResponseMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        #
-        $key = $request->fullUrl();
-        logger("about to cache");
-        logger($key);
-        return response()->json(Cache::remember($key, 300,
-                    function()use ($next,$request){
-                    $response = $next($request);
-                    return json_decode($response->getContent());
-                })
-            );
+    public function handle($request, Closure $next){
+        #todo add status code
+        $cached_response= Cache::get($this->getKey($request));
+        if($cached_response!=''){
+            return response()->json(json_decode($cached_response));
+        }
+        else{
+            return $next($request);
+        }
+
     }
+
+    public function terminate($request, $response){
+        logger("termianting");
+        logger($this->getkey($request));
+        Cache::add($this->getKey($request), $response->getContent(), 30*60);
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getKey($request)
+    {
+        return $request->fullUrl();
+    }
+
 }
