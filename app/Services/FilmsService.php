2c38b23b308c1cc36d0f0533f77d9dbbb4fc65cf
ilozuluchris <chidiuso@grit.systems>
2019-10-28 16:38:53 +0100

    Documentation for films service

--- app/Services/FilmsService.php
+++ app/Services/FilmsService.php
@@ -14,48 +14,60 @@ class FilmsService extends BaseNetworkService
 
     public function __construct(Client $HttpClient, CommentRepositoryInterface $repository)
     {
-        $this->repository = $repository;
+        $this->comment_repository = $repository;
         parent::__construct($HttpClient);
 
     }
 
-    /*** Fetch ALL FILMS
+    /*** Fetch all films
      * @return FilmResourceCollection
-     * @throws SwapiGetException
      */
     public function allFilms(){
         $json_content =  $this->getUrl('https://swapi.co/api/films/')['results'];
-        $films = $this->serializeFilmsData($json_content);
+        $films = $this->prepareFilmsData($json_content);
         return new FilmResourceCollection($films);
 
     }
 
+    /** Get content for film
+     * @param int $film_episode_id identifier for film
+     * @return array
+     */
     public function getFilmContents(int $film_episode_id){
         $json_content = $this->getUrl('https://swapi.co/api/films/'.$film_episode_id);
         return $json_content;
     }
 
-    /**
+    /** Add comment count to films
      * @param $json_content
-     *
+     * @return static
      */
     private function addCommentCount($json_content)
     {
-        $full_content = collect($json_content)->map(function ($item, $key) {
-            $item['comment_count'] = $this->repository->countForFilm($item['episode_id']);
+        $full_content = collect($json_content)->map(function ($item) {
+            $item['comment_count'] = $this->comment_repository->countForFilm($item['episode_id']);
             return $item;
         });
         return $full_content;
     }
 
-    /**
+    /** Prepare film data for display
      * @param $json_content
      * @return mixed
      */
-    private function serializeFilmsData($json_content)
+    private function prepareFilmsData($json_content)
     {
         $full_content = $this->addCommentCount($json_content);
-        $sorted_films = $full_content->sortBy('release_date')->values();
+        $sorted_films = $this->sortFilms($full_content);
         return $sorted_films;
+    }
+
+    /** Sort Films by release date.
+     * @param $full_content
+     * @return mixed
+     */
+    private function sortFilms($full_content)
+    {
+        return $full_content->sortBy('release_date')->values();
     }
 }
