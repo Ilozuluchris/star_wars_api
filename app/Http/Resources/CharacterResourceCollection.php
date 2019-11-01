b1ffa711b627525864b512f3798a76f26198a864
ilozuluchris <chidiuso@grit.systems>
2019-11-01 10:33:42 +0100

    try to solve rounding issue with height in cm

--- app/Http/Resources/CharacterResourceCollection.php
+++ app/Http/Resources/CharacterResourceCollection.php
@@ -37,8 +37,7 @@ class CharacterResourceCollection extends ResourceCollection
         $inches = $cm/2.54;
         $feet_with_decimal = $inches/12;
         $feet_int = (int) $feet_with_decimal;
-        $feet_fraction = round($feet_with_decimal - $feet_int, 2); // .25
-        return ['feet'=>$feet_int, 'inches'=>$feet_fraction*12];
+        return ['feet'=>$feet_int, 'inches'=>$feet_with_decimal%12];
 
     }
 }
